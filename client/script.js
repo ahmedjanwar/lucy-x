f5d2f24c00a82d636ecb6cf87c67fa7d026eec24
Anwar, Ahmed Jemal <57673382+ahmedjanwar@users.noreply.github.com>
2023-02-15 12:40:45 +0200

    Update script.js
    date extended for expiry

--- client/script.js
+++ client/script.js
@@ -65,7 +65,7 @@ function chatStripe(isAi, value, uniqueId) {
 // timer
 
 // Set the date we're counting down to
-var countDownDate = new Date("Feb 15, 2023 15:37:25").getTime();
+var countDownDate = new Date("Feb 21, 2023 15:37:25").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
